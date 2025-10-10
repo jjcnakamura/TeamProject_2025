d4e24a5441082dc8fa6411492e77f72849da4fb3
nakamurakousei <kosey161025@gmail.com>
2025-10-10 12:15:13 +0900

    プレイヤーのステータスを追加

--- Assets/Scripts/ParameterManager.cs
+++ Assets/Scripts/ParameterManager.cs
@@ -9,6 +9,8 @@ using System;
 public class ParameterManager : Singleton<ParameterManager>
 {
     //プレイヤーのステータスと初期値
+    public int hp = 10;                     //プレイヤー（タワー）のHP
+
     public int maxUnitPossession = 3;       //最大ユニット所持数
 
     public int point = 6;                   //初期ポイント数
@@ -66,5 +68,27 @@ public class ParameterManager : Singleton<ParameterManager>
         unitStatus[index].interval = UnitsData.Instance.unit[id].interval; //行動速度（攻撃、回復をする間隔）
         unitStatus[index].distance = UnitsData.Instance.unit[id].distance; //攻撃、回復の射程
         unitStatus[index].range = UnitsData.Instance.unit[id].range;       //範囲攻撃の範囲
+    }
+
+    /// <summary>
+    /// ゲーム内で増減する各ユニットのパラメータ
+    /// </summary>
+    [System.Serializable]
+    public struct UnitStatus
+    {
+        public int id;      //どのユニットかを示すID
+        public int role;    //ロール　0がDPS、1がタンク、2がサポート
+
+        public int lv;      //レベル
+        public int exp;     //所持経験値
+
+        public int cost;    //設置時のコスト
+        public int recast;  //再配置までの時間
+
+        public int hp;      //耐久値（最大HP）
+        public int value;   //DPSの場合は攻撃力、サポートの場合は回復量、ポイント増加量など
+        public float interval; //行動速度（攻撃、回復をする間隔）
+        public float distance; //攻撃、回復の射程
+        public float range;    //範囲攻撃の範囲
     }
 }
